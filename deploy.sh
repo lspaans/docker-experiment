ca1a1b68c29dbf7fdba124908f395587b6e6a28e
Leon Spaans <lspaans@gmail.com>
2014-11-12 22:04:52 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# deploy.sh: Idempotent script deploying a Docker-experiment
+#
+
+set -e
+
+install_packages () {
+    while [ $# -ne 0 ] ; do
+        if [ -e /usr/share/doc/"$1" ] ; then
+            printf 'Package already installed: %s\n' "$1" >&2
+            shift
+        else
+            yum install -y "$@"
+            break
+        fi
+    done
+}
+
+install_script () {
+    SCRIPT=/usr/local/bin/"$1"
+    printf "Installing %s...\n" "$SCRIPT" >&2
+    cat > "$SCRIPT"
+    chmod 755 "$SCRIPT"
+}
+
+install_script redeploy <<'EOF'
+#!/bin/sh
+sudo sh /vagrant/deploy.sh
+EOF
+
+yum update -y
+
+install_packages \
+    docker-io
+
+chkconfig docker on
+
+service docker start
+
+cd /vagrant/src
+docker build -t vagrant/centos-node-hello .
+docker run -p 49160:8080 -d vagrant/centos-node-hello
